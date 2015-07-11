7f6248bc85f2f6d1f45ccb242f6d519361e8006c
Ali Mihandoost <i@ali.md>
2015-07-12 00:21:56 +0430

    file: improbe file api

--- files.js
+++ files.js
@@ -1,9 +1,20 @@
 import fs from 'fs';
 
-export function write(file, json) {
-  return fs.writeFileSync(`stores/${file}.json`, JSON.stringify(json, null, 2));
+export function write(file, data) {
+  console.log(`write ${file}`);
+  let
+  filePath = `stores/${file}.json`,
+  json = JSON.stringify(data, null, 2);
+  console.log(json);
+  return fs.writeFileSync(filePath, json);
 }
 
 export function read(file) {
-  return JSON.parse(fs.readFileSync(`stores/${file}.json`));
+  console.log(`Read ${file}`);
+  let
+  filePath = `stores/${file}.json`,
+  fileContent = fs.readFileSync(filePath),
+  data = JSON.parse(fileContent);
+  console.log(fileContent);
+  return data;
 }
