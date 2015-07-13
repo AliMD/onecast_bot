dd5d8a86b757536f60a75688b89b6559a0db5e5b
Ali Mihandoost <i@ali.md>
2015-07-13 21:54:34 +0430

    files: fix make file inf not exist bug

--- files.js
+++ files.js
@@ -1,23 +1,20 @@
 import fs from 'fs';
 
 export function write(file, data, store = './stores') {
-  console.log(`write ${file}`);
-
-  let
-  filePath = `${store}/${file}.json`,
-  json = JSON.stringify(data, null, 2);
+  let filePath = `${store}/${file}.json`;
+  console.log(`write ${filePath}`);
+  let json = JSON.stringify(data, null, 2);
   console.log(`${data.length} item`);
   // console.log(json);
   return fs.writeFileSync(filePath, json);
 }
 
 export function read(file, defaultData, store = './stores') {
-  console.log(`Read ${file}`);
-
   let filePath = `${store}/${file}.json`;
+  console.log(`Read ${filePath}`);
   if(!fs.existsSync(filePath))
   {
-    write(file, defaultData);
+    write(file, defaultData, store);
     return defaultData;
   }
   let
