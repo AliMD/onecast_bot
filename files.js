4ddaf3e39cd131d0238f530ce7ddb6ccd8e3c676
Ali Mihandoost <i@ali.md>
2015-07-13 21:38:19 +0430

    files: improve api, make file on read and get stote location

--- files.js
+++ files.js
@@ -1,21 +1,29 @@
 import fs from 'fs';
 
-export function write(file, data) {
+export function write(file, data, store = './stores') {
   console.log(`write ${file}`);
+
   let
-  filePath = `stores/${file}.json`,
+  filePath = `${store}/${file}.json`,
   json = JSON.stringify(data, null, 2);
   console.log(`${data.length} item`);
   // console.log(json);
   return fs.writeFileSync(filePath, json);
 }
 
-export function read(file) {
+export function read(file, defaultData, store = './stores') {
   console.log(`Read ${file}`);
+
+  let filePath = `${store}/${file}.json`;
+  if(!fs.existsSync(filePath))
+  {
+    write(file, defaultData);
+    return defaultData;
+  }
   let
-  filePath = `stores/${file}.json`,
   fileContent = fs.readFileSync(filePath),
-  data = JSON.parse(fileContent);
+  data = JSON.parse(fileContent)
+  ;
   console.log(`${data.length} item`);
   // console.log(data);
   return data;
